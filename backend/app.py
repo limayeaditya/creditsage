f115b2a0e012943668bf55603ef81a9055a838b8
Aditya Limaye <52648711+limayeaditya@users.noreply.github.com>
2025-03-29 15:44:33 +0530

    fix: update rating after CRUD

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,9 @@ def add_mortgage(mortgage: MortgageCreate, db: Session = Depends(get_db)):
     try:
         result = insert_mortgage(db, mortgage)
         logger.info(f"New mortgage added: {result}")
-        return result
+        return {"message": "Mortgage updated successfully",
+                "Updated Credit Rating": result
+                }
     except Exception as e:
         logger.error(f"Error adding mortgage: {str(e)}")
         raise HTTPException(status_code=400, detail="Error processing mortgage")
@@ -32,13 +34,15 @@ def fetch_mortgages(db: Session = Depends(get_db)):
 @app.put("/mortgages/{mortgage_id}/", response_model=dict)
 def edit_mortgage(mortgage_id: int, updated_mortgage: MortgageCreate, db: Session = Depends(get_db)):
     try:
-        mortgage = update_mortgage(db, mortgage_id, updated_mortgage)
-        if not mortgage:
+        result = update_mortgage(db, mortgage_id, updated_mortgage)
+        if not result:
             logger.warning(f"Mortgage with ID {mortgage_id} not found for update")
             raise HTTPException(status_code=404, detail="Mortgage not found")
 
         logger.info(f"Mortgage updated: ID {mortgage_id}")
-        return {"message": "Mortgage updated successfully"}
+        return {"message": "Mortgage updated successfully",
+                "Updated Credit Rating": result
+                }
     except Exception as e:
         logger.error(f"Error updating mortgage {mortgage_id}: {str(e)}")
         raise HTTPException(status_code=400, detail="Error updating mortgage")
@@ -47,13 +51,15 @@ def edit_mortgage(mortgage_id: int, updated_mortgage: MortgageCreate, db: Sessio
 @app.delete("/mortgages/{mortgage_id}/", response_model=dict)
 def remove_mortgage(mortgage_id: int, db: Session = Depends(get_db)):
     try:
-        deleted = delete_mortgage(db, mortgage_id)
-        if not deleted:
+        result = delete_mortgage(db, mortgage_id)
+        if not result:
             logger.warning(f"Mortgage with ID {mortgage_id} not found for deletion")
             raise HTTPException(status_code=404, detail="Mortgage not found")
 
         logger.info(f"Mortgage with ID {mortgage_id} deleted successfully")
-        return {"message": "Mortgage deleted successfully"}
+        return {"message": "Mortgage updated successfully",
+                "Updated Credit Rating": result
+                }
     except Exception as e:
         logger.error(f"Error deleting mortgage {mortgage_id}: {str(e)}")
         raise HTTPException(status_code=500, detail="Could not delete mortgage")
